490607aa62aeba7c0f0bbffe9c827abde266983d
James Norris <james.norris.developer@gmail.com>
2026-07-31 08:59:31 -0400

    Added to the Dockerfile, updated app.py, and created the new Dockerfile.runtime

--- phases/00-setup-and-tooling/07-docker-for-ai/code/app.py
+++ phases/00-setup-and-tooling/07-docker-for-ai/code/app.py
@@ -1,10 +1,10 @@
-from flask importt Flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.roue("/")
+@app.route("/")
 def hello():
     return "Hello from ai-dev container!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=50000)
+    app.run(host="0.0.0.0", port=5000)
